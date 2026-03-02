d567ef8ed69cd7e9320ca7c612b85376b04745da
Minh Khoi Phan <pmka42@gmail.com>
2026-03-01 22:55:32 -0500

    Updated GRAPHQL_URI for deployment.

--- client/src/apollo/client.ts
+++ client/src/apollo/client.ts
@@ -1,7 +1,11 @@
 import { ApolloClient, InMemoryCache, HttpLink } from "@apollo/client";
 
+const GRAPHQL_URI = import.meta.env.PROD 
+  ? "https://game-keeper.vercel.app/graphql" 
+  : "http://localhost:5000/graphql";
+
 const httpLink = new HttpLink({
-  uri: "http://localhost:5000/graphql", 
+  uri: GRAPHQL_URI, 
   credentials: "include", 
 });
 
